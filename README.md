eeedd98ba4115b119db6f76d652f0f1f35f91187
tim <myasoedov.tim@gmail.com>
2024-11-26 00:33:38 +0300

    add variables and mqtt

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # be-bot-api
 
+Install libs: pip install -r requirements.txt
 Start project: uvicorn be_bot_api.api.main:app
--- src/be_bot_api/api/main.py
+++ src/be_bot_api/api/main.py
@@ -1,10 +1,25 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from fastapi_mqtt import FastMQTT, MQTTConfig
+from dotenv import dotenv_values
+
+# Загрузка переменных окружения.
+env_vars = dotenv_values(".env")
 
 app = FastAPI(
-        title="bebot",
-        version="1.0.0"
+        title=env_vars.get("APP_NAME"),
+        version=env_vars.get("APP_VERSION")
     )
+
+mqtt_config = MQTTConfig()
+mqtt_config.host = env_vars.get("MQTT_HOST")
+mqtt_config.port = env_vars.get("MQTT_PORT")
+mqtt_config.username = env_vars.get("MQTT_USER")
+mqtt_config.password = env_vars.get("MQTT_PASSWORD")
+
+mqtt = FastMQTT(
+    config=mqtt_config
+)
  
 @app.get("/")
 def read_root():
