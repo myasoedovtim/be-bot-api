1e381885b7e7f4d24897d9f99e40fdbaa3bbde6a
tim <myasoedov.tim@gmail.com>
2024-12-16 01:19:16 +0300

    rename getsensor

--- src/be_bot_api/api/main.py
+++ src/be_bot_api/api/main.py
@@ -98,7 +98,7 @@ async def send_command_backward(device_id: str, value: int):
     return {"detail": "Ok"}
 
 @app.get("/bebot/api/v1.0/command/getsensor/{device_id}")
-async def send_command_backward(device_id: str, sensor_type: str):
+async def send_command_getsensor(device_id: str, sensor_type: str):
     if device_id in devices:
         mqtt.publish("/bebot/device/"+device_id, {"getsensor": sensor_type})
     else:
