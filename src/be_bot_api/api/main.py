84a056224b6a707f01451d29ede48bc20910a90c
tim <myasoedov.tim@gmail.com>
2025-02-02 17:13:20 +0300

    comments

--- src/be_bot_api/api/main.py
+++ src/be_bot_api/api/main.py
@@ -1,3 +1,4 @@
+# Импорт библиотек
 from fastapi import FastAPI, Body, status, HTTPException
 from fastapi.responses import JSONResponse
 from fastapi_mqtt import FastMQTT, MQTTConfig
@@ -41,6 +42,7 @@ tags_metadata = [
     },
 ]
 
+# Общее описание
 description = """
 BeBot API предназначено для взаимодействия с внешними устройствами.
 
@@ -56,6 +58,7 @@ BeBot API предназначено для взаимодействия с вн
 # Загрузка переменных окружения.
 env_vars = dotenv_values(".env")   
 
+# Создание объекта класса FastAPI, который предоставляет всю функциональность для API.
 app = FastAPI(
         title=env_vars.get("APP_NAME"),
         version=env_vars.get("APP_VERSION"),
@@ -67,33 +70,39 @@ app = FastAPI(
         openapi_tags=tags_metadata
     )
 
+# Создание объекта с конфигурацией подключения к брокеру mqtt.
 mqtt_config = MQTTConfig()
 mqtt_config.host = env_vars.get("MQTT_HOST")
 mqtt_config.port = env_vars.get("MQTT_PORT")
 mqtt_config.username = env_vars.get("MQTT_USER")
 mqtt_config.password = env_vars.get("MQTT_PASSWORD")
 
+# Создание объекта класса FastMQTT, который предоставляет функциональность для взаимодействия с брокером mqtt.
 mqtt = FastMQTT(
     config=mqtt_config
 )
 
-
+# Объявление словаря (ключ-значение) для хранения устройств.
 devices = {}
 
+# Подключение к брокеру mqtt по событию - старт приложения.
 @app.on_event("startup")
 async def startapp():
     await mqtt.connection()
 
+# Отключение от брокеру mqtt по событию - завершение приложения.
 @app.on_event("shutdown")
 async def shutdown():
     await mqtt.client.disconnect()
 
+# Подписываемся на определенные очереди брокера по событию - подключение к брокеру.
 @mqtt.on_connect()
 def connect(client, flags, rc, properties):
     mqtt.client.subscribe("/bebot/to/api/init")
     mqtt.client.subscribe("/bebot/to/api/status")
     print("Connected to mqtt: ", client, flags, rc, properties)
 
+ # Обработка входящего сообщения (событие on_message).
 @mqtt.on_message()
 async def message(client, topic, payload, qos, properties):
     print("Received message: ",topic, payload.decode(), qos, properties)
@@ -105,23 +114,27 @@ async def message(client, topic, payload, qos, properties):
         devices[device["device_id"]]["is_active"] = device["is_active"]
     
 
-
+# Отображение сообщения на устройстве при событии брокера on_disconnect.
 @mqtt.on_disconnect()
 def disconnect(client, packet, exc=None):
     print("Disconnected from mqtt")
 
+# Отображение сообщения на устройстве при событии брокера on_subscribe.
 @mqtt.on_subscribe()
 def subscribe(client, mid, qos, properties):
     print("subscribed", client, mid, qos, properties)
- 
+
+# Методы API.
 @app.get("/")
 def read_root():
     return ({"hello": "i'm bebot!"})
 
+# Получить список зарегистированных устройств.
 @app.get("/bebot/api/v1.0/devices", tags=["get devices"])
 def get_devices():
     return devices
 
+# Добавить устройство в  список зарегистированных.
 @app.post("/bebot/api/v1.0/devices", status_code=status.HTTP_201_CREATED, tags=["post devices"])
 def add_device(device : Device):
     if device.device_id in devices:
@@ -129,12 +142,14 @@ def add_device(device : Device):
     devices[device.device_id] = device
     return device
 
+# Получить данные устройства по идентификатору.
 @app.get("/bebot/api/v1.0/devices/{device_id}", response_model=Device, tags=["get device by id"])
 async def get_device(device_id: str):
     if device_id not in devices:
         raise HTTPException(status_code=404, detail="Device not found")
     return devices[device_id]
 
+# Отправить команду forward для устройства в брокер.
 @app.put("/bebot/api/v1.0/command/forward/{device_id}/{value}", tags=["put forward"])
 async def send_command_forward(device_id: str, value: int):
     if device_id in devices:
@@ -143,6 +158,7 @@ async def send_command_forward(device_id: str, value: int):
         raise HTTPException(status_code=404, detail="Device not found")
     return {"detail": "Ok"}
 
+# Отправить команду turn для устройства в брокер.
 @app.put("/bebot/api/v1.0/command/turn/{device_id}/{value}", tags=["put turn"])
 async def send_command_turn(device_id: str, value: int):
     if device_id in devices:
@@ -151,6 +167,7 @@ async def send_command_turn(device_id: str, value: int):
         raise HTTPException(status_code=404, detail="Device not found")
     return {"detail": "Ok"}
 
+# Отправить команду stop для устройства в брокер.
 @app.put("/bebot/api/v1.0/command/stop/{device_id}", tags=["put stop"])
 async def send_command_stop(device_id: str):
     if device_id in devices:
@@ -159,6 +176,7 @@ async def send_command_stop(device_id: str):
         raise HTTPException(status_code=404, detail="Device not found")
     return {"detail": "Ok"}
 
+# Отправить команду backward для устройства в брокер.
 @app.put("/bebot/api/v1.0/command/backward/{device_id}/{value}", tags=["put backward"])
 async def send_command_backward(device_id: str, value: int):
     if device_id in devices:
@@ -167,6 +185,7 @@ async def send_command_backward(device_id: str, value: int):
         raise HTTPException(status_code=404, detail="Device not found")
     return {"detail": "Ok"}
 
+# Отправить команду на получение данных с сенсора устройства в брокер.
 @app.get("/bebot/api/v1.0/command/getsensor/{device_id}", tags=["get getsensor"])
 async def send_command_getsensor(device_id: str, sensor_type: str):
     if device_id in devices:
